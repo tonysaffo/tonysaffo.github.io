6f7a9534ab7cb805aa896515dcd51a3ba6374f7f
Neokkers <mendelson.inbox@gmail.com>
2020-02-23 01:04:21 +0200

    loyaltip build

---
+++ precache-manifest.4868652b511038352f4d041a9c7b3387.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f30c0bd789149c688422207faa3bf5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4433a09928e383b74fb9",
+    "url": "/static/js/2.6f9dbafe.chunk.js"
+  },
+  {
+    "revision": "8ae279e0396a82bcb324f2e1f01716e9",
+    "url": "/static/js/2.6f9dbafe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f349df16e518c3f965",
+    "url": "/static/js/main.916ba37c.chunk.js"
+  },
+  {
+    "revision": "eb6836e0a6ea9cb1b0a9",
+    "url": "/static/js/runtime-main.13399c6d.js"
+  }
+]);
